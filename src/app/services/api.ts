10899ffdc9dd2b940062367632534e7e21d75e2f
Julieta <julieta-ramos@estudiante.mdp.utn.edu.ar>
2025-10-22 09:27:34 -0300

    add footer component

--- src/app/services/api.ts
+++ src/app/services/api.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { environment } from '../../enviroments/enviroment';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environments';
 
 /**
  * @class ApiService
