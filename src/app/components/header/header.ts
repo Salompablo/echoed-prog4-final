fa03ec3cf563da1300297470af76a08966f76772
Manuel Palacios Inza <manuelpalaciosinzacontacto@gmail.com>
2025-10-23 20:33:46 -0300

    feat(header): Add profile dropdown menu
    Add user profile dropdown to header

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,10 +1,11 @@
 import { Component, inject } from '@angular/core';
 import { Theme } from '../../services/theme';
 import { SearchBar } from "../search-bar/search-bar";
+import { ProfileDropdown } from "../profile-dropdown/profile-dropdown";
 
 @Component({
   selector: 'app-header',
-  imports: [SearchBar],
+  imports: [SearchBar, ProfileDropdown],
   templateUrl: './header.html',
   styleUrl: './header.css'
 })
