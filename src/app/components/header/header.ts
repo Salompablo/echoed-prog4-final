02001c6e0394ab2f56df5a1cbab7be751a9a9e3c
Julieta <julieta-ramos@estudiante.mdp.utn.edu.ar>
2025-10-23 21:58:06 -0300

    Hide search bar on auth routes in header

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -2,14 +2,22 @@ import { Component, inject } from '@angular/core';
 import { Theme } from '../../services/theme';
 import { SearchBar } from "../search-bar/search-bar";
 import { ProfileDropdown } from "../profile-dropdown/profile-dropdown";
+import { Router } from '@angular/router';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-header',
-  imports: [SearchBar, ProfileDropdown],
+  imports: [SearchBar, ProfileDropdown, NgIf],
   templateUrl: './header.html',
   styleUrl: './header.css'
 })
 export class Header {
   public themeService = inject(Theme);
+  private router = inject(Router);
+
+  isAuthRoute() : any{
+    const currentUrl = this.router.url;
+    return currentUrl.includes('/register') || currentUrl.includes('/login');
+  }
 
 }
