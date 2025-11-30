4387a8ffe17b2c4982c3961dc5f24e0c4fcabe34
Julieta <julieta-ramos@estudiante.mdp.utn.edu.ar>
2025-11-29 23:28:13 -0300

    Moved language dropdown from header to footer

--- src/app/components/footer/footer.ts
+++ src/app/components/footer/footer.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
+import { LanguageDropdown } from '../language-dropdown/language-dropdown';
+
 
 @Component({
   selector: 'app-footer',
-  imports: [RouterLink, TranslateModule],
+  imports: [RouterLink, TranslateModule, LanguageDropdown],
   templateUrl: './footer.html',
   styleUrl: './footer.css'
 })
 export class Footer {
+
 
 }
